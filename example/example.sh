068c4f2bdb777217509cfde74281dd80898a2876
DW <49634738+zd105@users.noreply.github.com>
2019-07-01 20:45:58 +0800

    Add files via upload

--- example/example.sh
+++ example/example.sh
@@ -1,1 +1,1 @@
-perl ../AllEnricher -l example.glist -s hsa -v v20190612 -o ./allenricher -r /public/home/zhangdu/src/R-3.4.2/builddir/bin/Rscript -n KEGG+Reactome+DO+GO+DisGeNET
+perl ../AllEnricher -l example.glist -s hsa -v v20190612 -o ./allenricher -r /usr/bin/Rscript -n KEGG+Reactome+DO+GO+DisGeNET
